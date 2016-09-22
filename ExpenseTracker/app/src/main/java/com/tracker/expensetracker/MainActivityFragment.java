a83bf9fa3c6291adcc0243f5e085729e6c4d0ab8
phaki <phaki.rcumar@gmail.com>
2016-09-22 16:16:43 +0200

    created activity settings

---
+++ ExpenseTracker/app/src/main/java/com/tracker/expensetracker/MainActivityFragment.java
@@ -0,0 +1,15 @@
+package com.tracker.expensetracker;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by admin on 22.09.2016.
+ */
+public class MainActivityFragment extends Fragment {
+
+
+}
---
+++ ExpenseTracker/app/src/main/java/com/tracker/expensetracker/SettingsActivityFragment.java
@@ -0,0 +1,32 @@
+package com.tracker.expensetracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by admin on 22.09.2016.
+ */
+public class SettingsActivityFragment extends AppCompatActivity {
+
+    public SettingsActivityFragment() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_settings);
+
+        ExpandableListView fixedcosts = (ExpandableListView) findViewById(R.id.fixedcosts);
+        ExpandableListView categories = (ExpandableListView) findViewById(R.id.categories);
+        Button newFixedcost = (Button) findViewById(R.id.newFixedcost);
+        TextView totalFixedcosts = (TextView) findViewById(R.id.totalFixedcosts);
+
+
+    }
+}
