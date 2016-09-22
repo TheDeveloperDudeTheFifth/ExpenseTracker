1bb2d864bc939e71a204ec4a6ea383895a29f75e
Nicolas <gas@sevitec.ch>
2016-09-22 16:16:26 +0200

    tagesende

--- ExpenseTracker/app/src/main/java/com/tracker/expensetracker/Home_Screen.java
+++ ExpenseTracker/app/src/main/java/com/tracker/expensetracker/Home_Screen.java
@@ -1,32 +1,29 @@
 package com.tracker.expensetracker;
 
 
-import android.support.v4.app.FragmentTabHost;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TabHost;
 
 public class Home_Screen extends AppCompatActivity {
-    private TabHost mTabHost;
+    private TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__screen);
 
-        mTabHost = (TabHost)findViewById(R.id.tabHost);
-        mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
-            @Override
-            public void onTabChanged(String tabId) {
-                switch(tabId){
-                    case "home": setContentView(R.layout.home);
-                        break;
-                }
+        tabHost = (TabHost)findViewById(R.id.tabHost);
 
-            }
-        });
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("First Tab");
+
+        tab1.setIndicator("Tab1");
+        tab1.setContent(new Intent(this,Home_Screen.class));
 
 
+
+        tabHost.addTab(tab1);
 
     }
 
