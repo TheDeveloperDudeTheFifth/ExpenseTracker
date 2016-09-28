d6dfe565859ecafe627f65f024b2bf39e59a1f72
Nicolas <gas@sevitec.ch>
2016-09-28 09:29:17 +0200

    new menu

--- ExpenseTracker/app/src/main/java/com/tracker/expensetracker/Home_Screen.java
+++ ExpenseTracker/app/src/main/java/com/tracker/expensetracker/Home_Screen.java
@@ -1,34 +1,62 @@
 package com.tracker.expensetracker;
 
 
-import android.support.v4.app.FragmentTabHost;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TabHost;
 
 public class Home_Screen extends AppCompatActivity {
-    private TabHost mTabHost;
-
+    TabHost tabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__screen);
 
-        mTabHost = (TabHost)findViewById(R.id.tabHost);
-        mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
-            @Override
-            public void onTabChanged(String tabId) {
-                switch(tabId){
-                    case "home": setContentView(R.layout.home);
-                        break;
-                }
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
 
-            }
-        });
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Tab One");
+        host.addTab(spec);
 
+        //Tab 2
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Tab Two");
+        host.addTab(spec);
 
-
+        //Tab 3
+        spec = host.newTabSpec("Tab Three");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Tab Three");
+        host.addTab(spec);
+    }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
 }
 
