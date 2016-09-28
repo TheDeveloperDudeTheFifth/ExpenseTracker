59c28a096d208895cd1bfab557fe7c31c255a28c
Nicolas <gas@sevitec.ch>
2016-09-28 09:17:26 +0200

    Tabhost aDDED

--- ExpenseTracker/app/src/main/java/com/tracker/expensetracker/Home_Screen.java
+++ ExpenseTracker/app/src/main/java/com/tracker/expensetracker/Home_Screen.java
@@ -4,28 +4,59 @@ package com.tracker.expensetracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TabHost;
 
 public class Home_Screen extends AppCompatActivity {
-    private TabHost tabHost;
-
+    TabHost tabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__screen);
 
-        tabHost = (TabHost)findViewById(R.id.tabHost);
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
 
-        TabHost.TabSpec tab1 = tabHost.newTabSpec("First Tab");
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Tab One");
+        host.addTab(spec);
 
-        tab1.setIndicator("Tab1");
-        tab1.setContent(new Intent(this,Home_Screen.class));
+        //Tab 2
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Tab Two");
+        host.addTab(spec);
 
-
-
-        tabHost.addTab(tab1);
-
+        //Tab 3
+        spec = host.newTabSpec("Tab Three");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Tab Three");
+        host.addTab(spec);
+    }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
 }
 
