c5aaef5b93d4861459d5165188d9b3d2473df6ba
Nicolas <gas@sevitec.ch>
2016-09-28 11:09:07 +0200

    merge

--- ExpenseTracker/app/src/main/java/com/tracker/expensetracker/Home_Screen.java
+++ ExpenseTracker/app/src/main/java/com/tracker/expensetracker/Home_Screen.java
@@ -2,6 +2,7 @@ package com.tracker.expensetracker;
 
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,28 +14,39 @@ public class Home_Screen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home__screen);
+        setContentView(R.layout.tabhost);
 
         TabHost host = (TabHost)findViewById(R.id.tabHost);
         host.setup();
 
         //Tab 1
-        TabHost.TabSpec spec = host.newTabSpec("Tab One");
-        spec.setContent(R.id.tab1);
-        spec.setIndicator("Tab One");
+        TabHost.TabSpec spec = host.newTabSpec("Home");
+        spec.setContent(R.id.Home);
+        spec.setIndicator("home",Drawable.createFromPath());
         host.addTab(spec);
 
         //Tab 2
-        spec = host.newTabSpec("Tab Two");
-        spec.setContent(R.id.tab2);
-        spec.setIndicator("Tab Two");
+        spec = host.newTabSpec("Stats");
+        spec.setContent(R.id.Stats);
+        spec.setIndicator("Stats");
         host.addTab(spec);
 
         //Tab 3
-        spec = host.newTabSpec("Tab Three");
-        spec.setContent(R.id.tab3);
-        spec.setIndicator("Tab Three");
+        spec = host.newTabSpec("Map");
+        spec.setContent(R.id.Map);
+        spec.setIndicator("Map");
         host.addTab(spec);
+
+        spec = host.newTabSpec("Settings");
+        spec.setContent(R.id.settings);
+        spec.setIndicator("Settings");
+        host.addTab(spec);
+
+        spec = host.newTabSpec("Add");
+        spec.setContent(R.id.add);
+        spec.setIndicator("add");
+        host.addTab(spec);
+
     }
 /*
     @Override
